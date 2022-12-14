5e0253be47864d67a56185181c80b09b0dd684f1
SabeehHasnain <sabeehhusnain@gmail.com>
2022-12-14 12:41:17 +0500

    First Push

---
+++ assets/js/accordian.js
@@ -0,0 +1,39 @@
+const accordianBtns = document.querySelectorAll(".accordian");
+
+accordianBtns.forEach((accordian) => {
+  accordian.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
+
+const accordian_00Btns = document.querySelectorAll(".accordian_00");
+
+accordian_00Btns.forEach((accordian_00) => {
+  accordian_00.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
